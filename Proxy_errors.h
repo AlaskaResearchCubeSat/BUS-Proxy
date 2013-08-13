f86631cb195def10d3689801f7b0fd4f30371634
Jesse Frey <jmfrey@alaska.edu>
2013-08-13 15:43:14 -0800

    Added error codes for reset

---
+++ Proxy_errors.h
@@ -0,0 +1,10 @@
+#ifndef __BUS_ERRORS_H
+  #define __BUS_ERRORS_H
+  
+  //error sources for BUS test program
+  enum{ERR_SRC_CMD=ERR_SRC_SUBSYSTEM};
+    
+  //command errors
+  enum{CMD_ERR_RESET};
+#endif
+  
