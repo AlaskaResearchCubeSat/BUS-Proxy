bc7a65ec2dfca2c7b31450494f3706fe5af61300
Jesse Frey <jmfrey@alaska.edu>
2014-06-10 11:18:32 -0800

    Added __getchar function and added long integer format to scanf

--- main.c
+++ main.c
@@ -33,6 +33,11 @@ int __putchar(int c){
   }
 }
 
+//set printf and friends to send chars out UCA1 uart
+int __getchar(void){
+  return UCA1_Getc();
+}
+
 //handle subsystem specific commands
 int SUB_parseCmd(unsigned char src,unsigned char cmd,unsigned char *dat,unsigned short len){
   int i;
